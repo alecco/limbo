772f324b7a6e27811f731a26aa1e78638618fb2a
Alecco <alecco@ologan.com>
2025-04-13 23:11:50 +0200

    page_cache: fix eviction when tail is dirty/locked
    While there, evict as many as needed to make room for a new page.

--- core/storage/page_cache.rs
+++ core/storage/page_cache.rs
@@ -1,7 +1,7 @@
 use std::{cell::RefCell, collections::HashMap, ptr::NonNull};
 
 use std::sync::Arc;
-use tracing::{debug, trace};
+use tracing::{debug, trace, warn};
 
 use super::pager::PageRef;
 
@@ -63,6 +63,8 @@ impl DumbLruPageCache {
     pub fn insert(&mut self, key: PageCacheKey, value: PageRef) {
         self._delete(key.clone(), false);
         trace!("cache_insert(key={:?})", key);
+        // TODO: fail to insert if couldn't make room for page
+        let _ = self.evict();
         let entry = Box::new(PageCacheEntry {
             key: key.clone(),
             next: None,
@@ -74,9 +76,6 @@ impl DumbLruPageCache {
         self._insert_head(ptr);
 
         self.map.borrow_mut().insert(key, ptr);
-        if self.len() > self.capacity {
-            self.pop_if_not_dirty_or_locked();
-        }
     }
 
     pub fn delete(&mut self, key: PageCacheKey) {
@@ -139,6 +138,7 @@ impl DumbLruPageCache {
             return false;
         }
         self._detach(entry);
+        true
     }
 
     fn _detach(&mut self, mut entry: NonNull<PageCacheEntry>) {
@@ -171,7 +171,6 @@ impl DumbLruPageCache {
                 n_mut.prev = Some(p);
             },
         };
-        true
     }
 
     /// inserts into head, assuming we detached first
@@ -190,21 +189,50 @@ impl DumbLruPageCache {
         self.head.borrow_mut().replace(entry);
     }
 
-    fn pop_if_not_dirty_or_locked(&mut self) {
-        let tail = *self.tail.borrow();
-        if tail.is_none() {
-            return;
+    // Tries to evict pages until there's capacity for one more page
+    #[must_use]
+    fn evict(&mut self) -> bool {
+        if self.len() < self.capacity {
+            return true;
         }
-        let mut tail = tail.unwrap();
-        let tail_entry = unsafe { tail.as_mut() };
-        if tail_entry.page.is_dirty() || tail_entry.page.is_locked() {
-            // TODO: drop another clean and unlocked entry
-            return;
+
+        let mut current = match *self.tail.borrow() {
+            Some(tail) => tail,
+            None => {
+                debug!("evict() nothing at tail");
+                return false;
+            }
+        };
+
+        while self.len() >= self.capacity {
+            let current_entry = unsafe { current.as_mut() };
+
+            if current_entry.page.is_dirty() || current_entry.page.is_locked() {
+                match current_entry.prev {
+                    Some(prev) => current = prev,
+                    None => {
+                        debug!("evict() not enough clean and unlocked pages to evict");
+                        return false;
+                    }
+                }
+                continue;
+            }
+
+            debug!("evict(key={:?})", current_entry.key);
+
+            let prev = current_entry.prev;
+            let key_to_remove = current_entry.key.clone();
+            if !self.detach(current, true) {
+                warn!("evict(key={:?}) failed to detach", current_entry.key);
+                break;
+            }
+            assert!(self.map.borrow_mut().remove(&key_to_remove).is_some());
+            if prev.is_none() {
+                break;
+            }
+            current = prev.unwrap();
         }
-        tracing::debug!("pop_if_not_dirty(key={:?})", tail_entry.key);
-        if self.detach(tail, true) {
-            assert!(self.map.borrow_mut().remove(&tail_entry.key).is_some());
-        }
+        true
     }
 
     pub fn clear(&mut self) {
@@ -393,6 +421,41 @@ mod tests {
 
     fn page_has_content(page: &PageRef) -> bool {
         page.is_loaded() && page.get().contents.is_some()
+    }
+
+    #[derive(Clone, Copy)]
+    pub enum DirtyState {
+        Clean,
+        Dirty,
+    }
+
+    #[derive(Clone, Copy)]
+    pub enum LockState {
+        Unlocked,
+        Locked,
+    }
+
+    #[must_use]
+    fn insert_page_with_state(
+        cache: &mut DumbLruPageCache,
+        id: usize,
+        dirty: DirtyState,
+        lock: LockState,
+    ) -> PageCacheKey {
+        let key = create_key(id);
+        let page = page_with_content(id);
+
+        match dirty {
+            DirtyState::Dirty => page.set_dirty(),
+            DirtyState::Clean => (),
+        }
+
+        match lock {
+            LockState::Locked => page.set_locked(),
+            LockState::Unlocked => (),
+        }
+        cache.insert(key.clone(), page.clone());
+        key
     }
 
     #[test]
@@ -636,15 +699,6 @@ mod tests {
     }
 
     #[test]
-    fn test_page_cache_evict() {
-        let mut cache = DumbLruPageCache::new(1);
-        let key1 = insert_page(&mut cache, 1);
-        let key2 = insert_page(&mut cache, 2);
-        assert_eq!(cache.get(&key2).unwrap().get().id, 2);
-        assert!(cache.get(&key1).is_none());
-    }
-
-    #[test]
     fn test_page_cache_fuzz() {
         let seed = std::time::SystemTime::now()
             .duration_since(std::time::UNIX_EPOCH)
@@ -745,5 +799,69 @@ mod tests {
             let key = insert_page(&mut cache, i);
             assert_eq!(cache.peek(&key, false).unwrap().get().id, i);
         }
+    }
+
+    #[test]
+    fn test_page_cache_evict_one() {
+        let mut cache = DumbLruPageCache::new(1);
+        let key1 = insert_page(&mut cache, 1);
+        let _ = insert_page(&mut cache, 2);
+        assert!(cache.get(&key1).is_none());
+    }
+
+    #[test]
+    fn test_page_cache_no_evict_dirty() {
+        let mut cache = DumbLruPageCache::new(1);
+        let _ = insert_page_with_state(&mut cache, 1, DirtyState::Dirty, LockState::Unlocked);
+        assert!(cache.evict() == false);
+        assert!(cache.len() == 1);
+    }
+
+    #[test]
+    fn test_page_cache_no_evict_locked() {
+        let mut cache = DumbLruPageCache::new(1);
+        let _ = insert_page_with_state(&mut cache, 1, DirtyState::Clean, LockState::Locked);
+        assert!(cache.evict() == false);
+        assert!(cache.len() == 1);
+    }
+
+    #[test]
+    fn test_page_cache_no_evict_one_dirty() {
+        let mut cache = DumbLruPageCache::new(1);
+        let key1 = insert_page_with_state(&mut cache, 1, DirtyState::Dirty, LockState::Unlocked);
+        let _ = insert_page(&mut cache, 2);
+        assert!(cache.get(&key1).is_some());
+    }
+
+    #[test]
+    fn test_page_cache_no_evict_one_locked() {
+        let mut cache = DumbLruPageCache::new(1);
+        let key1 = insert_page_with_state(&mut cache, 1, DirtyState::Clean, LockState::Locked);
+        let _ = insert_page(&mut cache, 2); // fails to evict 1
+        assert!(cache.get(&key1).is_some());
+    }
+
+    #[test]
+    fn test_page_cache_evict_one_after_cleaned() {
+        let mut cache = DumbLruPageCache::new(1);
+        let key1 = insert_page_with_state(&mut cache, 1, DirtyState::Dirty, LockState::Unlocked);
+        let _ = insert_page(&mut cache, 2);
+        let entry1 = cache.get(&key1);
+        assert!(entry1.is_some());
+        entry1.unwrap().clear_dirty();
+        let _ = insert_page(&mut cache, 3);
+        assert!(cache.get(&key1).is_none());
+    }
+
+    #[test]
+    fn test_page_cache_evict_one_after_unlocked() {
+        let mut cache = DumbLruPageCache::new(1);
+        let key1 = insert_page_with_state(&mut cache, 1, DirtyState::Clean, LockState::Locked);
+        let _ = insert_page(&mut cache, 2);
+        let entry1 = cache.get(&key1);
+        assert!(entry1.is_some());
+        entry1.unwrap().clear_locked();
+        let _ = insert_page(&mut cache, 3);
+        assert!(cache.get(&key1).is_none());
     }
 }
