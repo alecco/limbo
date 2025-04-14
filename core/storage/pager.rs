e465f6c42ab538b2fe60286950a4d6df997c6579
Alecco <alecco@ologan.com>
2025-04-14 21:07:39 +0200

    pager: make Page Send and Sync

--- core/storage/pager.rs
+++ core/storage/pager.rs
@@ -26,6 +26,9 @@ pub struct Page {
     pub inner: UnsafeCell<PageInner>,
 }
 
+unsafe impl Send for Page {}
+unsafe impl Sync for Page {}
+
 // Concurrency control of pages will be handled by the pager, we won't wrap Page with RwLock
 // because that is bad bad.
 pub type PageRef = Arc<Page>;
